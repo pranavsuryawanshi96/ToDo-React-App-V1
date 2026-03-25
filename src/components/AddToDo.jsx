20c8a72e860c239e8a9ad2b3596ffa28dc24dd87
pranavsuryawanshi96 <pranavsuryawanshi61@gmail.com>
2026-03-25 21:42:30 +0530

    created react app

---
+++ src/components/AddToDo.jsx
@@ -0,0 +1,24 @@
+function AddToDo() {
+  return (
+    <div className="container text-center">
+      <div className="row">
+        <div className="col-6">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Todo here"
+          />
+        </div>
+        <div className="col-4">
+          <input type="date" className="form-control" />
+        </div>
+        <div className="col-2">
+          <button type="button" className="btn btn-success">
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default AddToDo;
